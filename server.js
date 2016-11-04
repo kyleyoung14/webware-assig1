6cadb5a3a1a1e60e8035c41c5bd20cc22c0c9e92
kyleyoung14 <kcyoung@wpi.edu>
2016-11-04 15:06:47 -0400

    css files working

--- server.js
+++ server.js
@@ -6,25 +6,34 @@ var http = require('http')
 var server = http.createServer (function (req, res) {
   var uri = url.parse(req.url)
 
-  // switch( uri.pathname ) {
-  //   case '/':
-  //     sendFile(res, 'fencing.html')
-  //     break
-  //   case '/fencing.html':
-  //     sendFile(res, 'fencing.html')
-  //     break
-  //   case '/index.html':
-  //     sendFile(res, 'index.html')
-  //     break
-  //   default:
-  //     res.end('404 not found')
+  switch( uri.pathname ) {
+    case '/':
+      sendFile(res, 'public/fencing.html')
+      break
+    case '/fencing.html':
+      sendFile(res, 'public/fencing.html')
+      break
+    case '/index.html':
+      sendFile(res, 'public/index.html')
+      break
+    case '/css/style.css':
+      sendFile(res, 'public/css/style.css', 'text/css')
+      break
+    case '/css/bootstrap.min.css':
+      sendFile(res, 'public/css/bootstrap.min.css', 'text/css')
+      break
+    case '/js/scripts.js':
+      sendFile(res, 'public/js/scripts.js', 'text/javascript')
+      break
+    default:
+      res.end('404 not found')
+  }
+  // if(uri.pathname == '/'){
+  //   sendFile(res, 'public/fencing.html')
   // }
-  if(uri.pathname == '/'){
-    sendFile(res, 'public/fencing.html')
-  }
-  else{
-    sendFile(res, '.' + uri.pathname)
-  }
+  // else{
+  //   sendFile(res, '.' + uri.pathname)
+  // }
 })
 
 server.listen(process.env.PORT || port);
@@ -32,10 +41,11 @@ console.log('listening on 8080')
 
 // subroutines
 
-function sendFile(res, filename) {
+function sendFile(res, filename, contentType) {
+  contentType = contentType || 'text/html';
 
   fs.readFile(filename, function(error, content) {
-    res.writeHead(200, {'Content-type': 'text/html'})
+    res.writeHead(200, {'Content-type': contentType})
     res.end(content, 'utf-8')
   })
 
